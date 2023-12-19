40da308231b7e8a60bcee30c15b75932dbb66499
EmmieIO <markonuoha6@gmail.com>
2023-12-19 20:20:38 +0100

    Creating our first livewire Component

---
+++ app/Livewire/Auth/Navigation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use Livewire\Component;
+
+class Navigation extends Component{
+
+    public $name = "Code with Emmie💥";
+
+    public function render()
+    {
+        return view('livewire.auth.navigation');
+    }
+}
