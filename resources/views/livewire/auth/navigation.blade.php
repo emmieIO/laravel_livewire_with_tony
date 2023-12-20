16a8d75ff09e107c27a987832b0ef7f583aa3102
EmmieIO <markonuoha6@gmail.com>
2023-12-20 03:46:04 +0100

    working with laravel model binding

--- resources/views/livewire/auth/navigation.blade.php
+++ resources/views/livewire/auth/navigation.blade.php
@@ -1,5 +1,4 @@
 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-end z-10">
-    <h1 href="" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>{{$name}}</h1>
     @auth
         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Dashboard</a>
     @else
